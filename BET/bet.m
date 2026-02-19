4ccfb6f0bed68a0a2c6f7ec61bce280908e81767
alirodriguezz <rodriguezamoalicia@gmail.com>
2026-02-19 13:20:01 +0100

    bet add: Ct, CPi, lambdas y global solidity

--- BET/bet.m
+++ BET/bet.m
@@ -9,6 +9,16 @@ function results = bet(params)
     % Outputs:
     %   results - structure containing the results of the analysis
     
+    % Thrust Coefficient
+    Ct = params.W / (params.rho * params.n_blades * params.Omega^2 * params.R^4);
+
+    % Induced Power Coefficient
+    CPi(:) = Ct/2*(sqrt(2*Ct+(Vz(:)/(Omega*R)).^2)+Vz(:)/(Omega*R));
+
+    % Lambdas
+    lambda_i(:) = 0.5*(sqrt(2*Ct+(Vz(:)/(Omega*R)).^2)-Vz(:)/(Omega*R));
+    lambda(:) = lambda_i(:) + Vz(:)/(Omega*R); 
     
-    
+    % Global solidity
+    sigma = @(x) params.n_blades*c(x)/(pi*params.R);
 end
