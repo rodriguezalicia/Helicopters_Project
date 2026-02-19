195f68607b2f5a7198306d6883463c70b81d1221
alirodriguezz <rodriguezamoalicia@gmail.com>
2026-02-19 13:07:28 +0100

    params.m definido y comienzo de bet.m

---
+++ BET/bet.m
@@ -0,0 +1,14 @@
+% =================================================================
+%    BET - Blade Element Theory for Helicopter Rotor Analysis
+% =================================================================
+
+function results = bet(params)
+    % BET - Blade Element Theory for Helicopter Rotor Analysis
+    % Inputs:
+    %   params - structure containing all the parameters for the analysis
+    % Outputs:
+    %   results - structure containing the results of the analysis
+    
+    
+    
+end
